92b4b9a915cba830f4bd4fa29df1c7ee5c43904e
Trent Brew <tbrew1023@gmail.com>
2019-05-12 01:30:02 -0500

    Added projects pages

---
+++ javascript/scripts.js
@@ -0,0 +1,59 @@
+$(window).ready(function() {
+  console.log("window is ready");
+
+  /*$(".banner1").hover(function() {
+    console.log("on");
+    $(this).css({
+      'background-image':"url('res/gifs/bart2.gif')",
+      "filter":"contrast(1)"
+    });
+  }, function() {
+    console.log("off");
+    $(this).css({
+      'background-image':"url('res/gifs/bart2.gif')",
+      "filter":"contrast(0) brightness(2)",
+    });
+  });
+
+  $(".banner2").hover(function() {
+    console.log("on");
+    $(this).css({
+      'background-image':"url('res/gifs/soniccd.gif')",
+      "filter":"contrast(1)"
+    });
+  }, function() {
+    console.log("off");
+    $(this).css({
+      'background-image':"url('res/gifs/soniccd.gif')",
+      "filter":"contrast(0) brightness(2)"
+    });
+  });
+
+  $(".banner3").hover(function() {
+    console.log("on");
+    $(this).css({
+      'background-image':"url('res/gifs/lockedout.gif')",
+      "filter":"contrast(1)"
+    });
+  }, function() {
+    console.log("off");
+    $(this).css({
+      'background-image':"url('res/gifs/lockedout.gif')",
+      "filter":"contrast(0) brightness(2)"
+    });
+  });
+
+  $(".banner4").hover(function() {
+    console.log("on");
+    $(this).css({
+      'background-image':"url('res/gifs/cool2.gif')",
+      "filter":"contrast(1)"
+    });
+  }, function() {
+    console.log("off");
+    $(this).css({
+      'background-image':"url('res/gifs/cool2.gif')",
+      "filter":"contrast(0) brightness(2)"
+    });
+  });*/
+});
