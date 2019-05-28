0ecac3e31c76cad15045dfd6777343a39d46aaa4
tbrew1023 <tbrew1023@gmail.com>
2019-05-27 20:54:43 -0500

    Fixed gallery

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,12 +1,17 @@
 $(window).ready(function() {
   console.log("window is ready");
   //console.log("port1 pos: " + $(".port1").getBoundingClientRect().top);
+});
 
+$(document).ready(function() {
+  console.log("height: " + document.body.clientHeight);
 });
 
 $(window).scroll(function() {
   var scroll = $(window).scrollTop();
-  //console.log(scroll);
+  //var scrollBottom = $(window).scrollTop() + $(window).height();
+  //console.log("scroll: " + scroll);
+  //console.log("scroll bottom: " + $(window).height() - scroll);
 
   $(".parallax").css({
     "opacity":((200-(scroll/2.5))/100),
@@ -14,7 +19,7 @@ $(window).scroll(function() {
   });
 
   $(".parallax h1").css({
-   // "margin-top":(40-(scroll/8))
+    "margin-top":(40-(scroll/10))
   });
 
   if(scroll >= 400) {
@@ -53,4 +58,23 @@ $(window).scroll(function() {
       });
     }, 300);
   }
+});
+
+//---gallery hover stuff---
+
+var container = document.querySelector(".drawing-banner");
+var mover = document.querySelector(".item-mover");
+
+container.addEventListener("mousemove", function(e) {
+  mover.style.backgroundPositionX = -e.offsetX/2 + "px";
+  mover.style.backgroundPositionY = -e.offsetY*2 + "px";
+});
+
+container.addEventListener("mouseenter", function() {
+  
+  setTimeout(function() {
+    mover.classList.add("no-more-slidey");
+    //container.removeEventListener("mouseenter");
+  }, 250);
+  
 });
